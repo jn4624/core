8e6e27064893e9b904581c746178fa3f8193ef2a
LimJiNa <jn4624@gmail.com>
2024-02-06 22:41:59 +0900

    @Configuration 어노테이션 - 스프링의 CGLIB 바이트코드 조작 라이브러리 사용 확인 테스트

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,16 @@ public class ConfigurationSingletonTest {
         assertThat(memberRepository1).isSameAs(memberRepository);
         assertThat(memberRepository2).isSameAs(memberRepository);
     }
+
+    /**
+     * 임의의 클래스를 생성하여 싱글톤을 보장하기 위함.
+     */
+    @Test
+    @DisplayName("@Configuration 어노테이션 - 스프링의 CGLIB 바이트코드 조작 라이브러리 사용 확인")
+    void configurationDeep() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = applicationContext.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
